41b98a4baa1b2623c45c2a6ccf24cf25752a1fd0
3s Studio <linardsliepenieks@gmail.com>
2023-03-18 20:26:18 +0200

    Added git reanimate library - used for sidebars everything seems to work - this is the point where the library was implemented and nothing has been broken... yet

--- App.js
+++ App.js
@@ -2,11 +2,11 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useFonts } from "expo-font";
+import "react-native-gesture-handler";
 
 //Database related imports
 import { signInAnonymously } from "firebase/auth";
 import { auth } from "./firebaseConfig.js";
-
 
 //Under here imports for screens contained in other files in the screens folder
 
@@ -24,20 +24,18 @@ import GigApplyScreen from "./screens/GigApplyScreen";
 import GigStartScreen from "./screens/GigStartScreen";
 import MapsTestScreen from "./screens/MapTestScreen.js";
 
-
 //Temporary screen for backend work
 import DatabaseTestingScreen from "./screens/DatabaseTestingScreen";
 
-
 //Imports and executes the getActiveGigs and getUser funtions, so that ActiveGigsScreen does not need to wait for data fetching to happen
-import { getActiveGigs, getUser, getOngoingGigs } from './api/api'
+import { getActiveGigs, getUser, getOngoingGigs } from "./api/api";
 
 async function initializeData() {
-	await getUser()
-	await getActiveGigs()
-	await getOngoingGigs()
+	await getUser();
+	await getActiveGigs();
+	await getOngoingGigs();
 }
-initializeData()
+initializeData();
 
 //Initializes the stack navigator module, used to navigate between screens
 const Stack = createNativeStackNavigator();
@@ -58,14 +56,13 @@ export default function App() {
 
 	// This function autenthicates to firebase, allowing the app to access the database
 	signInAnonymously(auth)
-  		.then(() => {
-    		console.log('Auth successful');
-  		})
-  		.catch((error) => {
-    		const errorCode = error.code;
-    		const errorMessage = error.message;
-  		});
-
+		.then(() => {
+			console.log("Auth successful");
+		})
+		.catch((error) => {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+		});
 
 	return (
 		<NavigationContainer>
@@ -79,14 +76,12 @@ export default function App() {
 				<Stack.Screen name="Driving" component={DrivingScreen} />
 				<Stack.Screen name="Arrival" component={ArrivalScreen} />
 				<Stack.Screen name="AddPicInfo" component={AddPicsAndInfo} />
-				<Stack.Screen name="ForgotPassword" component={ForgotPassword}/>
-				<Stack.Screen name="GigApply" component={GigApplyScreen}/>
-				<Stack.Screen name="GigStart" component={GigStartScreen}/>
+				<Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+				<Stack.Screen name="GigApply" component={GigApplyScreen} />
+				<Stack.Screen name="GigStart" component={GigStartScreen} />
 
-				<Stack.Screen name="DBTest" component={DatabaseTestingScreen}/>	
-				<Stack.Screen name="MapTest" component={MapsTestScreen}/>	
-
-				
+				<Stack.Screen name="DBTest" component={DatabaseTestingScreen} />
+				<Stack.Screen name="MapTest" component={MapsTestScreen} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
--- screens/GigListScreen.js
+++ screens/GigListScreen.js
@@ -1,64 +1,95 @@
-import { View, Text, Pressable, SafeAreaView, FlatList } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, Pressable, SafeAreaView, FlatList } from "react-native";
+import React, { useState } from "react";
 
-import { STYLES } from '../constants/styles'
-import { availableGigsData } from '../api/api'
+import { STYLES } from "../constants/styles";
+import { availableGigsData } from "../api/api";
 
-
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 //This has the frontend code that shows either a list of available gigs or a text thing. Style accordingly
 function List(props) {
-  const navRef = props.nav
-  //This is the frontend code for an individual list item
-  const Item = ({id, startLocation, endLocation, reward, startTime, endTime}) => (
-    <View>
-      <Pressable onPress={() => onListItemPress(navRef)}> 
-        <Text>From {startLocation}</Text>
-        <Text>To {endLocation}</Text>
-        <Text>Reward {reward}€</Text>
-        <Text>Departure time: {startTime}</Text>
-        <Text>Arrival time: {endTime}</Text>
-      </Pressable>
-    </View>
-  )
-  if (availableGigsData.length === 0) {
-    return ( 
-      <Text>No available gigs</Text> 
-      )
-    } else {
-      return(
-        <FlatList
-        data={availableGigsData}
-        renderItem={({item}) => <Item 
-        startLocation={item.startLocation}
-        endLocation={item.endLocation}
-        reward={item.reward}
-        startTime={item.startTime}
-        endTime={item.endTime}
-        id={item.id}
-        />}
-        keyExtractor={item => item.id}
-        />
-        )
-      }
-    }
-    
-const GigListScreen = ({navigation}) => {
-      
-      //The List component is the function above. It returns the forntend code for available gigs list if there are available gigs
-      //Otherwise just returns text component telling the user that no gigs are available
-    return(
-    <SafeAreaView>
-      <List nav={navigation}/>
-    </SafeAreaView>
-  )
+	const navRef = props.nav;
+	//This is the frontend code for an individual list item
+	const Item = ({
+		id,
+		startLocation,
+		endLocation,
+		reward,
+		startTime,
+		endTime,
+	}) => (
+		<View>
+			<Pressable onPress={() => onListItemPress(navRef)}>
+				<Text>From {startLocation}</Text>
+				<Text>To {endLocation}</Text>
+				<Text>Reward {reward}€</Text>
+				<Text>Departure time: {startTime}</Text>
+				<Text>Arrival time: {endTime}</Text>
+			</Pressable>
+		</View>
+	);
+	if (1) {
+		//back in if availableGigsData.length === 0
+		return <Text>No available gigs</Text>;
+	} else {
+		return (
+			<FlatList
+				data={availableGigsData}
+				renderItem={({ item }) => (
+					<Item
+						startLocation={item.startLocation}
+						endLocation={item.endLocation}
+						reward={item.reward}
+						startTime={item.startTime}
+						endTime={item.endTime}
+						id={item.id}
+					/>
+				)}
+				keyExtractor={(item) => item.id}
+			/>
+		);
+	}
 }
 
-function onListItemPress(navigation){
-  navigation.navigate('GigApply')
+function Feed(nav) {
+	return (
+		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+			<SafeAreaView>
+				<List />
+				<Text> {/*nav={navigation}*/}</Text>
+			</SafeAreaView>
+		</View>
+	);
 }
 
+function Article() {
+	return (
+		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+			<Text>Article Screen</Text>
+		</View>
+	);
+}
 
+const Drawer = createDrawerNavigator();
 
-export default GigListScreen
+function MyDrawer() {
+	return (
+		<Drawer.Navigator useLegacyImplementation>
+			<Drawer.Screen name="Feed" component={Feed} />
+			<Drawer.Screen name="Article" component={Article} />
+		</Drawer.Navigator>
+	);
+}
+
+const GigListScreen = ({ navigation }) => {
+	//The List component is the function above. It returns the forntend code for available gigs list if there are available gigs
+	//Otherwise just returns text component telling the user that no gigs are available
+	return <MyDrawer />;
+};
+
+function onListItemPress(navigation) {
+	navigation.navigate("GigApply");
+}
+
+export default GigListScreen;
