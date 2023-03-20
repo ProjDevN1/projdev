33c653eca4ec440d506a60b17777a84dbe0d63e1
3s Studio <linardsliepenieks@gmail.com>
2023-03-20 20:43:22 +0200

    navigation

--- screens/GigApplyScreen.js
+++ screens/GigApplyScreen.js
@@ -1,4 +1,12 @@
-import { View, Text, Pressable, Alert, Linking, Button } from "react-native";
+import {
+	View,
+	Text,
+	Pressable,
+	Alert,
+	Linking,
+	Button,
+	Image,
+} from "react-native";
 import React, { useRef, useState } from "react";
 import MapView, { Circle, Marker } from "react-native-maps";
 import MapViewDirections from "react-native-maps-directions";
@@ -9,22 +17,24 @@ import { availableGigsData } from "../api/api";
 import { ELSTYLES } from "../constants/styles";
 import { STARTGIG } from "../constants/styles";
 
-const GOOGLE_MAPS_APIKEY = "AIzaSyBP6tdUhVPg34f1PfSR55r_eEZIrDAWsJo"
+const GOOGLE_MAPS_APIKEY = "AIzaSyBP6tdUhVPg34f1PfSR55r_eEZIrDAWsJo";
 
 const GigApplyScreen = ({ navigation }) => {
-
 	const currentGig = availableGigsData[clickedListItem];
 
-
 	// Here we will have a prompt with the phone number of the client
-	const showContactInfo = () => 
-	Alert.alert("Client contact information", "this alert is not yet functional", [
-		{text: "Cancel", style: "cancel"},
-		{
-			text:"Client phone number",
-			style: "cancel",
-		},
-	])
+	const showContactInfo = () =>
+		Alert.alert(
+			"Client contact information",
+			"this alert is not yet functional",
+			[
+				{ text: "Cancel", style: "cancel" },
+				{
+					text: "Client phone number",
+					style: "cancel",
+				},
+			]
+		);
 
 	// Code used for moving the map to the start and end locations
 	const mapRef = useRef(null);
@@ -33,25 +43,25 @@ const GigApplyScreen = ({ navigation }) => {
 		longitude: -0.0899163,
 		latitudeDelta: 0.01,
 		longitudeDelta: 0.01,
-	  });
+	});
 	const startData = {
 		latitude: currentGig.startLat,
 		longitude: currentGig.startLon,
 		latitudeDelta: 0.01,
 		longitudeDelta: 0.01,
-	}
+	};
 	const endData = {
 		latitude: currentGig.endLat,
 		longitude: currentGig.endLon,
 		latitudeDelta: 0.01,
 		longitudeDelta: 0.01,
-	}
+	};
 	const goToStart = () => {
-		mapRef.current.animateToRegion(startData, 3000)
+		mapRef.current.animateToRegion(startData, 3000);
 	};
 	const goToEnd = () => {
-		mapRef.current.animateToRegion(endData, 3000)
-	}
+		mapRef.current.animateToRegion(endData, 3000);
+	};
 
 	return (
 		<View style={STARTGIG.screenWrapper}>
@@ -68,43 +78,58 @@ const GigApplyScreen = ({ navigation }) => {
 						longitude: currentGig.startLon,
 						latitudeDelta: 0.0922,
 						longitudeDelta: 0.0421,
-
 					}}
 					onRegionChangeComplete={(region) => setRegion(region)}>
-					<Marker coordinate={startData}/>
-					<Marker coordinate={endData}/>
+					<Marker coordinate={startData} />
+					<Marker coordinate={endData} />
 					{/*Renders the route for the gig on the map screen*/}
 					<MapViewDirections
-						origin= {currentGig.startCoord}
+						origin={currentGig.startCoord}
 						destination={currentGig.endCoord}
 						apikey={GOOGLE_MAPS_APIKEY}
 						strokeWidth={3}
 						strokeColor="blue"
 						optimizeWaypoints={true}
 						onStart={(params) => {
-							console.log(`Started routing between "${params.origin}" and "${params.destination}"`)
+							console.log(
+								`Started routing between "${params.origin}" and "${params.destination}"`
+							);
 						}}
-						onReady={result => {
-							console.log(`Distance: ${result.distance} km`)
-							console.log(`Duration: ${result.duration} min`)
+						onReady={(result) => {
+							console.log(`Distance: ${result.distance} km`);
+							console.log(`Duration: ${result.duration} min`);
 						}}
 					/>
 				</MapView>
-				<Button on onPress={() => goToStart()} title="Go to start"/>
-				<Button on onPress={() => goToEnd()} title="Go to end"/>
+				<View style={STARTGIG.mapNavBtnWarpper}>
+					<Pressable
+						style={[ELSTYLES.button, { padding: 4 }]}
+						onPress={() => goToStart()}>
+						<Text style={{ textAlign: "center" }}>Origin</Text>
+					</Pressable>
+					<Pressable
+						style={[ELSTYLES.button, { padding: 4 }]}
+						onPress={() => goToEnd()}>
+						<Text>Destination</Text>
+					</Pressable>
+				</View>
 			</View>
 			{/*Search/info buttons - absolute element */}
 			<View style={STARTGIG.infoBtnWrapper}>
-				<Pressable style={ELSTYLES.buttonRound}>
-					<Text>S</Text>
-				</Pressable>
-
 				<Pressable
 					style={ELSTYLES.buttonRound}
 					onPress={() => {
 						Search(navigation);
 					}}>
-					<Text>F</Text>
+					<Image
+						style={{ width: "50%", height: "50%" }}
+						source={require("../assets/icons/searchIco.png")}></Image>
+				</Pressable>
+
+				<Pressable style={ELSTYLES.buttonRound}>
+					<Image
+						style={{ width: "50%", height: "50%" }}
+						source={require("../assets/icons/settingIco.png")}></Image>
 				</Pressable>
 			</View>
 			<View style={STARTGIG.infoWrapper}>
@@ -134,11 +159,15 @@ const GigApplyScreen = ({ navigation }) => {
 				</View>
 
 				<View style={STARTGIG.buttonWrapper}>
-					<Pressable style={[ELSTYLES.buttonAlt, STARTGIG.buttonStart]}>
+					<Pressable
+						style={[ELSTYLES.buttonAlt, STARTGIG.buttonStart]}
+						onPress={() => Apply(navigation)}>
 						<Text style={ELSTYLES.buttonAltTxt}>Apply</Text>
 					</Pressable>
 
-					<Pressable style={[ELSTYLES.buttonAlt, STARTGIG.buttonStart]} onPress={showContactInfo}>
+					<Pressable
+						style={[ELSTYLES.buttonAlt, STARTGIG.buttonStart]}
+						onPress={showContactInfo}>
 						<Text style={ELSTYLES.buttonAltTxt}>Contact info</Text>
 					</Pressable>
 				</View>
@@ -149,6 +178,9 @@ const GigApplyScreen = ({ navigation }) => {
 
 const Search = (navigation) => {
 	navigation.navigate("GigList");
+};
+const Apply = (navigation) => {
+	navigation.navigate("AddPicInfo");
 };
 
 export default GigApplyScreen;
--- screens/GigStartScreen.js
+++ screens/GigStartScreen.js
@@ -1,4 +1,12 @@
-import { View, Text, Pressable, Linking, Alert, Button } from "react-native";
+import {
+	View,
+	Text,
+	Pressable,
+	Linking,
+	Alert,
+	Button,
+	Image,
+} from "react-native";
 import { ELSTYLES } from "../constants/styles";
 import { STARTGIG } from "../constants/styles";
 import React, { useState, useRef } from "react";
@@ -8,26 +16,28 @@ import MapViewDirections from "react-native-maps-directions";
 import { clickedListItem, clientName } from "../screens/ActiveGigsScreen";
 import { activeGigsData } from "../api/api";
 
-const GOOGLE_MAPS_APIKEY = "AIzaSyBP6tdUhVPg34f1PfSR55r_eEZIrDAWsJo"
+const GOOGLE_MAPS_APIKEY = "AIzaSyBP6tdUhVPg34f1PfSR55r_eEZIrDAWsJo";
 
 /*This screen is very similar to the GigApplyScreen, because apparently the 
 only thing changing is that the apply-button changes to start-button*/
 
-const GigStartScreen = () => {
+const GigStartScreen = ({ navigation }) => {
 	const currentGig = activeGigsData[clickedListItem];
 	console.log(clickedListItem);
 
-
 	// Here we will have a prompt with the phone number of the client
-	const showContactInfo = () => 
-	Alert.alert("Client contact information", "this alert is not yet functional", [
-		{text: "Cancel", style: "cancel"},
-		{
-			text:"Client phone number",
-			style: "cancel",
-		},
-
-	])
+	const showContactInfo = () =>
+		Alert.alert(
+			"Client contact information",
+			"this alert is not yet functional",
+			[
+				{ text: "Cancel", style: "cancel" },
+				{
+					text: "Client phone number",
+					style: "cancel",
+				},
+			]
+		);
 	// Code used for moving the map to the start and end locations
 	const mapRef = useRef(null);
 	const [region, setRegion] = useState({
@@ -35,25 +45,25 @@ const GigStartScreen = () => {
 		longitude: -0.0899163,
 		latitudeDelta: 0.01,
 		longitudeDelta: 0.01,
-	  });
+	});
 	const startData = {
 		latitude: currentGig.startLat,
 		longitude: currentGig.startLon,
 		latitudeDelta: 0.01,
 		longitudeDelta: 0.01,
-	}
+	};
 	const endData = {
 		latitude: currentGig.endLat,
 		longitude: currentGig.endLon,
 		latitudeDelta: 0.01,
 		longitudeDelta: 0.01,
-	}
+	};
 	const goToStart = () => {
-		mapRef.current.animateToRegion(startData, 3000)
+		mapRef.current.animateToRegion(startData, 3000);
 	};
 	const goToEnd = () => {
-		mapRef.current.animateToRegion(endData, 3000)
-	}
+		mapRef.current.animateToRegion(endData, 3000);
+	};
 
 	return (
 		<View style={STARTGIG.screenWrapper}>
@@ -68,41 +78,60 @@ const GigStartScreen = () => {
 					initialRegion={{
 						latitude: currentGig.startLat,
 						longitude: currentGig.startLon,
-						latitudeDelta: 0.0922,
-						longitudeDelta: 0.0421,
-
+						latitudeDelta: 0.005,
+						longitudeDelta: 0.005,
 					}}
 					onRegionChangeComplete={(region) => setRegion(region)}>
-					<Marker coordinate={startData}/>
-					<Marker coordinate={endData}/>
+					<Marker coordinate={startData} />
+					<Marker coordinate={endData} />
 					{/*Renders the route for the gig on the map screen*/}
 					<MapViewDirections
-						origin= {currentGig.startCoord}
+						origin={currentGig.startCoord}
 						destination={currentGig.endCoord}
 						apikey={GOOGLE_MAPS_APIKEY}
 						strokeWidth={3}
 						strokeColor="blue"
 						optimizeWaypoints={true}
 						onStart={(params) => {
-							console.log(`Started routing between "${params.origin}" and "${params.destination}"`)
+							console.log(
+								`Started routing between "${params.origin}" and "${params.destination}"`
+							);
 						}}
-						onReady={result => {
-							console.log(`Distance: ${result.distance} km`)
-							console.log(`Duration: ${result.duration} min`)
+						onReady={(result) => {
+							console.log(`Distance: ${result.distance} km`);
+							console.log(`Duration: ${result.duration} min`);
 						}}
 					/>
 				</MapView>
-				<Button on onPress={() => goToStart()} title="Go to start"/>
-				<Button on onPress={() => goToEnd()} title="Go to end"/>
 			</View>
 			{/*Search/info buttons - absolute element */}
 			<View style={STARTGIG.infoBtnWrapper}>
-				<Pressable style={ELSTYLES.buttonRound}>
-					<Text>S</Text>
+				<Pressable
+					style={ELSTYLES.buttonRound}
+					onPress={() => {
+						Search(navigation);
+					}}>
+					<Image
+						style={{ width: "50%", height: "50%" }}
+						source={require("../assets/icons/searchIco.png")}></Image>
 				</Pressable>
 
 				<Pressable style={ELSTYLES.buttonRound}>
-					<Text>F</Text>
+					<Image
+						style={{ width: "50%", height: "50%" }}
+						source={require("../assets/icons/settingIco.png")}></Image>
+				</Pressable>
+			</View>
+			<View style={STARTGIG.mapNavBtnWarpper}>
+				<Pressable
+					style={[ELSTYLES.button, { padding: 4 }]}
+					onPress={() => goToStart()}>
+					<Text style={{ textAlign: "center" }}>Origin</Text>
+				</Pressable>
+				<Pressable
+					style={[ELSTYLES.button, { padding: 4 }]}
+					onPress={() => goToEnd()}>
+					<Text>Destination</Text>
 				</Pressable>
 			</View>
 
@@ -120,8 +149,10 @@ const GigStartScreen = () => {
 						<Text style={ELSTYLES.titleLlight}>{currentGig.title}</Text>
 					</View>
 					<View style={[STARTGIG.section, { justifyContent: "space-evenly" }]}>
-						<Text style={ELSTYLES.txtAlt}>{currentGig.leaveTime}</Text>
-						<Text style={ELSTYLES.txtAlt}>{currentGig.arrivalTime}</Text>
+						<Text style={ELSTYLES.txtAlt}>Leave: {currentGig.leaveTime}</Text>
+						<Text style={ELSTYLES.txtAlt}>
+							Arrive: {currentGig.arrivalTime}
+						</Text>
 						<Text style={ELSTYLES.txtAlt}>Pay: {currentGig.reward} €</Text>
 					</View>
 					<View style={[STARTGIG.section, { justifyContent: "space-evenly" }]}>
@@ -135,13 +166,18 @@ const GigStartScreen = () => {
 						<Text style={ELSTYLES.buttonAltTxt}>Start</Text>
 					</Pressable>
 
-					<Pressable style={[ELSTYLES.buttonAlt, STARTGIG.buttonStart]} onPress={showContactInfo}>
+					<Pressable
+						style={[ELSTYLES.buttonAlt, STARTGIG.buttonStart]}
+						onPress={showContactInfo}>
 						<Text style={ELSTYLES.buttonAltTxt}>Contact info</Text>
 					</Pressable>
 				</View>
 			</View>
 		</View>
 	);
+};
+const Search = (navigation) => {
+	navigation.navigate("GigList");
 };
 
 export default GigStartScreen;
